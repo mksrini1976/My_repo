c64cce5b16f7cc2be2e30ae04fba4335f7ed4041
sunetha <s.sunetha@gmail.com>
2020-03-15 02:54:02 +0000

    changed again prg2.txt in prerit BRANCH

--- prg2.py
+++ prg2.py
@@ -1,5 +1,5 @@
 To understand this example, you should have the knowledge of the following Python programming topics:
-====================================================
+=============trip to USA=======================================
 Python Input, Output and Import
 Python Data Types
 Python Operators
